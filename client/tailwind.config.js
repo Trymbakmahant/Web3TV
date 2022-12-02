c005f6c3b0a0e9f63d25e60c9886a7a879c45235
Trymbak Mahant <trymbak0102mahant@gmail.com>
2022-12-03 02:52:13 +0530

    navbar is added

---
+++ client/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      spacing: {
+        112: "28rem",
+        128: "32rem",
+      },
+    },
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+    darkTheme: "forest",
+  },
+};
