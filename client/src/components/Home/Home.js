93cd67c8ea3d4eeccd2706c8a7a9f89c04575edd
Trymbak Mahant <trymbak0102mahant@gmail.com>
2022-12-03 22:56:08 +0530

    automation added  to home

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,52 +1,43 @@
+import { useEffect, useState, useContext } from "react";
 import Card from "../card/card";
 import Sidebar from "../sidebar/Sidebar";
+import { AppContext } from "../context/AddressContext";
 
- const Logo = "/logo512.png";
- const objects= [
-    {
-      id: "1",
-      name: "Jsonis",
-      description:
-        "The InterPlanetary File System (IPFS) is a protocol, hypermedia and file sharing peer-to-peer network",
-      thumbnail:
-        "https://www.google.com/imgres?imgurl=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fen%2F3%2F34%2FCars_2006.jpg&imgrefurl=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FCars_(film)&tbnid=lwBg-owBejYAzM&vet=12ahUKEwiK0LGxhNn7AhWKk9gFHYJsDOkQMygHegUIARDtAQ..i&docid=eZItEn5mnMS3IM&w=259&h=383&q=cars&ved=2ahUKEwiK0LGxhNn7AhWKk9gFHYJsDOkQMygHegUIARDtAQ",
-      videoUrl: "https://www.youtube.com/watch?v=tA8CW9wFY28",
-    },
-    {
-      id: "2",
-      name: "Jis",
-      description: "The  hypermedia and file sharing peer-to-peer network",
-      thumbnail:
-        "https://www.google.com/imgres?imgurl=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fen%2F3%2F34%2FCars_2006.jpg&imgrefurl=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FCars_(film)&tbnid=lwBg-owBejYAzM&vet=12ahUKEwiK0LGxhNn7AhWKk9gFHYJsDOkQMygHegUIARDtAQ..i&docid=eZItEn5mnMS3IM&w=259&h=383&q=cars&ved=2ahUKEwiK0LGxhNn7AhWKk9gFHYJsDOkQMygHegUIARDtAQ",
-      videoUrl: "https://www.youtube.com/watch?v=tA8CW9wFY28",
-    },
-    {
-      id: "3",
-      name: "Json",
-      description:
-        "The  File System (IPFS) is a protocol, hypermedia and file sharing peer-to-peer network",
-      thumbnail:
-        "https://www.google.com/imgres?imgurl=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fen%2F3%2F34%2FCars_2006.jpg&imgrefurl=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FCars_(film)&tbnid=lwBg-owBejYAzM&vet=12ahUKEwiK0LGxhNn7AhWKk9gFHYJsDOkQMygHegUIARDtAQ..i&docid=eZItEn5mnMS3IM&w=259&h=383&q=cars&ved=2ahUKEwiK0LGxhNn7AhWKk9gFHYJsDOkQMygHegUIARDtAQ",
-      videoUrl: "https://www.youtube.com/watch?v=tA8CW9wFY28",
-    },
-  ];
+const Logo = "/logo512.png";
 
 function Home() {
+  const [videos, setVideos] = useState([]);
+  const ctx = useContext(AppContext);
+  const contract = ctx.sharedState.contract.contractData;
+
+  useEffect(() => {
+    if (contract !== null) {
+      (async function () {
+        const allVideos = await contract.showContent();
+        setVideos(allVideos);
+      })();
+    }
+  }, [contract]);
+
   return (
     <div>
       <Sidebar />
-      <div style = {{display: "inline-block", marginLeft: "7%"}}>
-      {objects.map(Jsonis => {
-        return <Card
-          key={Jsonis.id}
-          name={Jsonis.name}
-          description={Jsonis.description}
-          thumbnail={Logo}
-          id={Jsonis.id}
-        />
-      })
-      }
-      </div>
+      {videos.length > 0 && (
+        <div style={{ display: "inline-block", marginLeft: "7%" }}>
+          {videos.map((video, index) => {
+            return (
+              <Card
+                key={index}
+                name={video.name}
+                description={video.description}
+                thumbnail={Logo}
+                id={video.playbackID}
+                creator={video.creator}
+              />
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 }
