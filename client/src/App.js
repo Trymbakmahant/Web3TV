e2685fcd1efd04dd77e57661c6a933e3b7921eb0
Trymbak Mahant <trymbak0102mahant@gmail.com>
2022-12-04 05:07:21 +0530

    Livestream added

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,52 @@
-import logo from "./logo.svg";
 import "./App.css";
-
 import Navbar from "./components/Pages/Navbar";
-import Path from "./components/routes/Path";
 import Footer from "./components/Footer/Footer";
+import Path from "./components/routes/Path";
+import WorldCoin from "./components/Pages/WorldCoin";
+import { ethers } from "ethers";
+import { useEffect, useState, useContext } from "react";
+import {
+  LivepeerConfig,
+  createReactClient,
+  studioProvider,
+} from "@livepeer/react";
+import { AppContext } from "./components/context/AddressContext";
+const client = createReactClient({
+  provider: studioProvider({ apiKey: "300b138b-5f81-4e7f-9426-fa5b898d4374" }),
+});
+
 function App() {
+  const [isVerified, setIsVerified] = useState(false);
+  const ctx = useContext(AppContext);
+
+  const contract = ctx.sharedState.contract.contractData;
+  const accountAddress = ctx.sharedState.contract.accountAddress;
+
+  useEffect(() => {
+    if (accountAddress) {
+      (async function () {
+        const verification = await contract.isVerified(accountAddress);
+        setIsVerified(verification);
+      })();
+    }
+  }, [accountAddress]);
+
   return (
     <>
-      <Navbar />
-
-      <Path />
-      <Footer />
+      {isVerified ? (
+        <div>
+          <LivepeerConfig client={client}>
+            <Navbar />
+            <Path />
+            <Footer />
+          </LivepeerConfig>
+        </div>
+      ) : (
+        <div>
+          <Navbar />
+          <WorldCoin />
+        </div>
+      )}
     </>
   );
 }
